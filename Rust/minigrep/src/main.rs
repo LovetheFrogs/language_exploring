7e2c58ac91a503087750cafd207f0d97c68e4925
LovetheFrogs <102818341+LovetheFrogs@users.noreply.github.com>
2023-02-15 12:38:57 +0100

    Moved logic to lib.rs

--- Rust/minigrep/src/main.rs
+++ Rust/minigrep/src/main.rs
@@ -1,7 +1,18 @@
-// Bring into scope std::env to make use of the std::env::args function.
 use std::env;
+use std::process;
+
+use minigrep::Config;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    dbg!(args);
+    
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        eprintln!("Problem parsing arguments: {err}");
+        process::exit(1);
+    });
+
+    if let Err(e) = minigrep::run(config) {
+        eprintln!("Application error: {e}");
+        process::exit(1);
+    }
 }
