c87a6b6c5b1e7e2d1beb91b5985d7207b7ff6dee
LovetheFrogs <102818341+LovetheFrogs@users.noreply.github.com>
2023-02-15 11:23:16 +0100

    Created integration test in testing folder

---
+++ Rust/rust/tests/integration_test.rs
@@ -0,0 +1,14 @@
+/** Integration tests are placed on a diferent folder called `tests` located in the same level as the src folder. Inside, you create
+ *  your files for integrated tests like this one. Note that each file in this directory is its own crate. To run this file in
+ *  particular, use the `cargo test --test integration_test` command. Also, only projects with a lib.rs file can use this tests/ 
+ *  folder function, as this library exposes functions that other crates can use. 
+ */
+use testing;
+
+mod common;
+
+#[test]
+fn it_adds_two() {
+    common::setup();
+    assert_eq!(4, testing::add_two(2));
+}
