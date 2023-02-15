fa566530dd2c5a1ef237ee95dd1567a3511739af
LovetheFrogs <102818341+LovetheFrogs@users.noreply.github.com>
2023-02-15 11:23:02 +0100

    Added common file for testing crate

---
+++ Rust/rust/tests/common/mod.rs
@@ -0,0 +1,6 @@
+/** We can use the common folder to specify functions that multiple test files use and not test this function. We can call this file
+ *  by using `mod common;` in our test files.
+ */
+pub fn setup() {
+    // -snip-
+}
