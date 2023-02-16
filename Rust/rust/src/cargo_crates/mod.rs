3e2613afba0b94cbb9ebb2d998f3d20a43bb2059
LovetheFrogs <102818341+LovetheFrogs@users.noreply.github.com>
2023-02-16 16:13:04 +0100

    Implemented Chapters 14.1 and 14.2

--- Rust/rust/src/cargo_crates/mod.rs
+++ Rust/rust/src/cargo_crates/mod.rs
@@ -31,3 +31,55 @@
 pub fn add_one(x: i32) -> i32 {
     x + 1
 }
+
+/* To make it easier for users of your crate, you should create a public API with `pub use`. This simplifies the imports and can change an import like
+ *          use my_crate::some_module::another_module::UsefulType;
+ * to something easier to locate like
+ *          use my_crate::UsefulType;
+ * The example below shows how you can simplify the import in a crate named arts.
+ */
+
+/*
+//! Art
+//! 
+//! A library for modeling artistic concepts.
+*/
+
+/* This will simplify imports for users of the public API, allowing them to call the commands
+ *          use art::mix
+ *          use art::PrimaryColor;
+ */
+pub use self::kinds::PrimaryColor;
+pub use self::kinds::SecondaryColor;
+pub use self::utils::mix;
+
+pub mod kinds {
+    /// The primary colors according to the RYB color model.
+    pub enum PrimaryColor {
+        Red,
+        Yellow,
+        Blue,
+    }
+
+    /// The secondary colors acording to the RYB color model.
+    pub enum SecondaryColor {
+        Orange,
+        Green,
+        Purple,
+    }
+}
+
+pub mod utils {
+    use crate::cargo_crates::kinds::*;
+
+    use super::kinds::{PrimaryColor, SecondaryColor};
+
+    /// Combines two primary colors in equal amounts to create
+    /// a secondary color.
+    pub fn mix(c1: PrimaryColor, c2: PrimaryColor) -> SecondaryColor {
+        // --snip--
+        SecondaryColor::Green
+    }
+}
+
+/* Chapter 14.3-Cargo Workspaces can be found in folder add, located in languague_exploring/Rust/add */
